3dfb21ad8a99d4fe516439001d51cfa45346714f
Alex <alexsandr498887@gmail.com>
2018-03-07 02:02:29 +0400

    v10

--- app.py
+++ app.py
@@ -22,11 +22,13 @@ def nedelia():
     else:
         return '2'
 
-def make_message():
+def make_message(mode):
     url = "http://raspisanie.asu.edu.ru/student/%D0%97%D0%9821/"+nedelia()
     urllib.request.urlopen(url).read()
-    
-    day=days[now_time().weekday()+1]
+    if mode=='Сегодня':
+        day=days[now_time().weekday()]
+    if mode=='Завтра':
+        day=days[now_time().weekday()+1]
     url = "http://raspisanie.asu.edu.ru/student/%D0%97%D0%9821/"+nedelia()
     text = urllib.request.urlopen(url).read().decode('utf-8')
     if day!='Воскресенье':
@@ -41,15 +43,22 @@ vk_api = vk.API(session, v='5.62')
 l={'95881708':None,'147933155':None,'164138740':None}
 d={'alex__brin':None,'ar4ibald98':None}
 while True:
+    for msg in vk_api.messages.get(count=10)['items']:
+            if msg['read_state']==0: # get all unread messages
+                if 'сегодня' in msg['body'].lower():
+                    print(vk_api.messages.send(user_id=msg['user_id'], message=make_message('Сегодня')))
+                if 'завтра' in msg['body'].lower():
+                    print(vk_api.messages.send(user_id=msg['user_id'], message=make_message('Сегодня')))
+                vk_api.messages.markAsRead(message_ids=msg['id'])
     for i in l:
         if list(now_time().timetuple())[3]>22 and list(now_time().timetuple())[3]<24 and (l[i]==None or ((now_time()-l[i]).seconds // 3600)>22):
-            print(vk_api.messages.send(user_id=i, message=make_message()))
+            print(vk_api.messages.send(user_id=i, message=make_message('Завтра')))
             l[i]=now_time()
     for i in d:
         if list(now_time().timetuple())[3]>22 and list(now_time().timetuple())[3]<24 and (d[i]==None or ((now_time()-d[i]).seconds // 3600)>22):
-            print(vk_api.messages.send(domain=i, message=make_message()))
+            print(vk_api.messages.send(domain=i, message=make_message('Завтра')))
             d[i]=now_time()
-    sleep(300)
+    sleep(10)
         
         
     
