2c07065357d45c9e8d068386fc675b4f1ee8def9
Alex <alexsandr498887@gmail.com>
2018-03-07 01:47:21 +0400

    v9

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import vk
 from time import *
 import datetime
 import urllib.request
-
+p_start=['','8:30','10:10','11:50','13:40','15:20','16:55','18:30','20:05']
 days=['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье']
 login = '+79969127739'
 password = '01qi1976'
@@ -30,18 +30,25 @@ def make_message():
     url = "http://raspisanie.asu.edu.ru/student/%D0%97%D0%9821/"+nedelia()
     text = urllib.request.urlopen(url).read().decode('utf-8')
     if day!='Воскресенье':
-        return 'Завтра к '+str(text[text.index(day)+len(day)+46:text.index(day)+len(day)+47])+' паре'
+        para=str(text[text.index(day)+len(day)+46:text.index(day)+len(day)+47])
+        return 'Завтра к '+p_start[int(para)]+' пара №'+para
     else:
         return 'Завтра нет пар' 
 
 session = vk.AuthSession(vk_id, login, password, scope='messages')
 vk_api = vk.API(session, v='5.62')
 
+l={'95881708':None,'147933155':None,'164138740':None}
+d={'alex__brin':None,'ar4ibald98':None}
 while True:
-    last_time=None
-    if list(now_time().timetuple())[3]>22 and list(now_time().timetuple())[3]<24 and (last_time==None or ((now_time()-last_time).seconds // 3600)>22):
-        print(vk_api.messages.send(user_id="95881708", message=make_message()))
-        last_time=now_time()
+    for i in l:
+        if list(now_time().timetuple())[3]>22 and list(now_time().timetuple())[3]<24 and (l[i]==None or ((now_time()-l[i]).seconds // 3600)>22):
+            print(vk_api.messages.send(user_id=i, message=make_message()))
+            l[i]=now_time()
+    for i in d:
+        if list(now_time().timetuple())[3]>22 and list(now_time().timetuple())[3]<24 and (d[i]==None or ((now_time()-d[i]).seconds // 3600)>22):
+            print(vk_api.messages.send(domain=i, message=make_message()))
+            d[i]=now_time()
     sleep(300)
         
         
